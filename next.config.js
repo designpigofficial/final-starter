0642385e2d5782c84b35ae4efe951cffe4ca265b
designpigofficial <designpig@outlook.com>
2022-08-16 18:11:43 -0600

    ci: setup

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-const withPlugins = require("next-compose-plugins");
-
-module.exports = withPlugins([], {});
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+    /* config options here */
+  }
+  
+  module.exports = nextConfig
